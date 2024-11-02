1bfa2d1c4adda63955d2c2bcb5cd039298354de8
xmaj2001 <xmaj2001@gmail.com>
2024-11-02 11:35:01 +0100

    update

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Routes from './Routes.tsx'
+import Routes from './routes.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
