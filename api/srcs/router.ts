d4a14bed0874e1e4b32b04c22881956ac0d44b71
xmaj2001 <xmaj2001@gmail.com>
2024-11-25 15:31:03 +0100

    update x5

--- api/srcs/router.ts
+++ api/srcs/router.ts
@@ -1,8 +1,11 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import controller from "./controller";
 const routers = Router();
 
 routers.get("/", controller.index);
+routers.get("/home", (req: Request, res: Response) => {
+    res.send("ola");
+});
 routers.post("/api/uploadfile", controller.uploadfile);
 routers.post("/api/downloadfile", controller.downloadfile);
 
