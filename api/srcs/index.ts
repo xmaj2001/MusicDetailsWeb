2ca4f0a020662f43a22a1ccf10103383cd420374
xmaj2001 <xmaj2001@gmail.com>
2024-11-25 15:06:46 +0100

    update x

--- api/srcs/index.ts
+++ api/srcs/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import routers from "./router";
 import cors from 'cors'
+import dotenv from 'dotenv'
 
 const app = express();
-const port = 3000;
+dotenv.config();
+const port = process.env.PORT || 3001;
 app.use(express.json({limit:'20mb'}));
 app.use(cors());
 
