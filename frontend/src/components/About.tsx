66194c4b315144ce3379b09c09d401d1c5b27df3
X-MAJ <117451874+xmaj2001@users.noreply.github.com>
2025-01-14 12:39:09 +0100

    update x

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -1,6 +1,7 @@
 import { Button, Link, Spacer } from "@nextui-org/react";
 import { motion } from "framer-motion";
-import { FaDownload } from "react-icons/fa6";
+import { BsMicrosoft } from "react-icons/bs";
+import { FaDownload, FaLinux } from "react-icons/fa6";
 import { FaUpload } from "react-icons/fa6";
 
 export default function About() {
@@ -21,35 +22,32 @@ export default function About() {
             className="header-sobre hidden sm:block">
 
             <div className="sobre mt-3">
+                <h3> Download</h3>
                 <motion.h2>Music Details</motion.h2>
-                <h3> Editor de Etiquetas de Música</h3>
+                <h3> Editor de Etiquetas de Música </h3>
                 <hr className="my-2" />
                 <p className="max-w-[500px] text-left">
-                    <p>
-                        E uma <strong>ferramenta </strong> que permite a{" "}
-                        <strong>edição de etiquetas de música</strong>, oferecendo uma maneira fácil de{" "}
-                        <strong>organizar e gerenciar informações importantes</strong> relacionadas às suas músicas.
-                    </p>
-                    <Spacer y={1} />
-                    <p>
-                        Com ela você pode <strong>personalizar e aprimorar</strong> as etiquetas das suas músicas como{" "}
-                        <strong>título</strong>, <strong>artista</strong>, <strong>álbum</strong>, <strong>ano de lançamento</strong>,{" "}
-                        <strong>gênero</strong>, <strong>compositor</strong>, <strong>grupo</strong>, <strong>BPM</strong>,{" "}
-                        <strong>copyright</strong> {""}
-                        adicionar, remover ou atualizar a  <strong> capa (cover)</strong> da música.
-                    </p>
+                    O Music Details é uma ferramenta prática e intuitiva para gerenciar e editar informações de suas músicas.<br />
+                    Disponível <b>versão desktop</b>,
+                    projetada especialmente para o sistema operacional: <span className="">
+                        <Link href="https://github.com/xmaj2001/MusicDetails" className="text-orange-500 font-bold">Window </Link>
+                        <span className="mx-1">e</span>
+                        <Link href="https://github.com/xmaj2001/musictag" className="text-orange-500 font-bold"> Linux</Link>.
+                    </span>
                 </p>
-                <Link href="/editor">
-                    <Button className="bg-orange-500 hover:bg-white hover:text-orange-500 mx-2">
-                        <FaUpload /> Upload File
+
+                <Link href="https://github.com/xmaj2001/musictag/archive/refs/tags/v1.0.0.zip">
+                    <Button className="bg-white hover:bg-orange-500 hover:text-white text-orange-500 mx-2" >
+                        <FaLinux /> Baixar
                     </Button>
                 </Link>
 
                 <Link href="https://github.com/xmaj2001/MusicDetails/archive/refs/tags/v1.0.0.zip">
-                    <Button className="bg-white hover:bg-orange-500 hover:text-white text-orange-500 mx-2" >
-                        <FaDownload /> Baixar
+                    <Button className="bg-orange-500 hover:bg-orange-500 hover:text-white text-white mx-2" >
+                        <BsMicrosoft /> Baixar
                     </Button>
                 </Link>
+                
             </div>
 
         </motion.div>
--- frontend/src/components/AboutApp.tsx
+++ frontend/src/components/AboutApp.tsx
@@ -1,6 +1,9 @@
-import { Button, Link } from "@nextui-org/react";
+import { Button, Link, Spacer } from "@nextui-org/react";
 import { motion } from "framer-motion";
 import { BiWindow } from "react-icons/bi";
+import { BsMicrosoft } from "react-icons/bs";
+import { FaLinux } from "react-icons/fa";
+import { FaDownload, FaUpload } from "react-icons/fa6";
 import { LiaLinux } from "react-icons/lia";
 
 export default function AboutApp() {
@@ -21,27 +24,24 @@ export default function AboutApp() {
             className="header-sobre hidden sm:block">
 
             <div className="sobre mt-3">
-                <h3> Download</h3>
-                <motion.h2>Music Details</motion.h2>
-                <h3> Editor de Etiquetas de Música </h3>
+            <motion.h2>Music Details</motion.h2>
+                <h3> Editor de Etiquetas de Música</h3>
                 <hr className="my-2" />
                 <p className="max-w-[500px] text-left">
-                    O Music Details é uma ferramenta prática e intuitiva para gerenciar e editar informações de suas músicas.<br />
-                    Disponível tanto na versão web, que você pode acessar diretamente pelo navegador,
-                    quanto na <Link href="https://github.com/xmaj2001/MusicDetails" className="text-orange-500 font-bold">versão  desktop</Link>, projetada especialmente para o sistema operacional Windows e linux.
+                    <p>
+                        E uma <strong>ferramenta </strong> que permite a{" "}
+                        <strong>edição de etiquetas de música</strong>, oferecendo uma maneira fácil de{" "}
+                        <strong>organizar e gerenciar informações importantes</strong> relacionadas às suas músicas.
+                    </p>
+                    <Spacer y={1} />
+                    <p>
+                        Com ela você pode <strong>personalizar e aprimorar</strong> as etiquetas das suas músicas como{" "}
+                        <strong>título</strong>, <strong>artista</strong>, <strong>álbum</strong>, <strong>ano de lançamento</strong>,{" "}
+                        <strong>gênero</strong>, <strong>compositor</strong>, <strong>grupo</strong>, <strong>BPM</strong>,{" "}
+                        <strong>copyright</strong> {""}
+                        adicionar, remover ou atualizar a  <strong> capa (cover)</strong> da música.
+                    </p>
                 </p>
-
-                <Link href="https://github.com/xmaj2001/MusicDetails/archive/refs/tags/v1.0.0.zip">
-                    <Button className="bg-white hover:bg-orange-500 hover:text-white text-orange-500 mx-2" >
-                        <LiaLinux /> Baixar 1.0.0
-                    </Button>
-                </Link>
-
-                <Link href="https://github.com/xmaj2001/MusicDetails/archive/refs/tags/v1.0.0.zip">
-                    <Button className="bg-white hover:bg-orange-500 hover:text-white text-orange-500 mx-2" >
-                        <BiWindow /> Baixar 1.0.0
-                    </Button>
-                </Link>
 
             </div>
 
